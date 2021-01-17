3238a77590737000ac27135251bfa0418517df24
Eevi Rutanen <57459056+eevirutanen@users.noreply.github.com>
2021-01-17 21:09:38 +0200

    Update sketch.js

--- WEEK_4/DAY_2/loopyLetters/sketch.js
+++ WEEK_4/DAY_2/loopyLetters/sketch.js
@@ -22,7 +22,7 @@ function draw(){
     noFill();
     stroke(255);
     //move to center
-    translate(180, 350);
+    translate(120, 400);
     //map mouse coordinates
     var mx = map(mouseX,0,width,-5,5);
     var my = map(mouseY,0,height,-5,5);

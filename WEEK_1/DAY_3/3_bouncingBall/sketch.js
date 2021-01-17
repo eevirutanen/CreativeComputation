da763a67dd3d996db015e5b9d29349006b756a91
Eevi Rutanen <57459056+eevirutanen@users.noreply.github.com>
2021-01-17 18:56:39 +0200

    Update sketch.js

--- WEEK_1/DAY_3/3_bouncingBall/sketch.js
+++ WEEK_1/DAY_3/3_bouncingBall/sketch.js
@@ -1,3 +1,9 @@
+//Looping animation using consitional statements.
+//Modify the bouncing ball sketch so that the ball moves sideways in addition to up and down.
+//1. Make a variable for x position
+//2. Make different variables for vertical and horizontal speeds: call them xSpeed and ySpeed
+//3. Check boundaries for the x coordinate in the same way as for y coordinate previously: Inverse xSpeed if x is smaller than 0 or larger than width. Inverse ySpeed if y is smaller than 0 or larger than height. Remember to take into account the size of the ball!
+	
 var y; //variable for y position 
 var w = 100; //size of the ball
 var speed = 2; //variable for speed
@@ -8,8 +14,8 @@ function setup(){
 }
 
 function draw(){
-    background(0); // redraw background every frame
-    fill(255,0,0);
+    background(255,255,0); // redraw background every frame
+    fill(255,200,255);
     ellipse(width/2, y, w, w); //draw ellipse using y and w
     y = y+speed; //increase y with speed
     //check boundaries:
@@ -19,11 +25,3 @@ function draw(){
         speed = speed * (-1);
     }
 }
-
-//EXERCISE:
-//Modify the bouncing ball sketch so that the ball moves sideways in addition to up and down
-//1. Make a variable for x position
-//2. Make different variables for vertical and horizontal speeds: call them xSpeed and ySpeed
-//3. Check boundaries for the x coordinate in the same way as for y coordinate previously: Inverse xSpeed if x is smaller than 0 or larger than width. Inverse ySpeed if y is smaller than 0 or larger than height. Remember to take into account the size of the ball!
-	
-

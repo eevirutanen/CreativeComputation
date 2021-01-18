717ff9d466be222ec57d7b352f7ac0942d10589f
Eevi Rutanen <57459056+eevirutanen@users.noreply.github.com>
2021-01-18 13:42:15 +0200

    Update sketch.js

--- WEEK_1/DAY_2/2_mouseDraw/sketch.js
+++ WEEK_1/DAY_2/2_mouseDraw/sketch.js
@@ -1,17 +1,14 @@
-//This sketch demonstrates how to make a simple drawing tool with mouse interaction
-//Eevi Rutanen
+//This sketch demonstrates how to make a simple drawing tool with mouse interaction.
+//Try drawing with different shapes, colors and strokes. Try updating the background in the draw() function.
+//Eevi Rutanen 15.10.2020
 function setup(){
     //set canvas size 500*500px
     createCanvas(500,500);
     strokeWeight(1); //set stroke weight to 1px
-    background(0); //draw black background
-    angleMode(DEGREES);
+    background(255,0,0); //draw red background
+    stroke(255,0,0); //set red stroke color
 }
 function draw(){
-    background(0); //draw black background
-    //draw ellipse at cursor position
-    noStroke();
-    //fill(width-mouseX,100,100);
-    fill(height-mouseY,100,100);
-    ellipse(mouseX,mouseY,100,100);
+    fill(255,200,255); //set pink fill color
+    ellipse(mouseX,mouseY,60); //draw 60px circle at cursor position
 }
